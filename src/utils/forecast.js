dddeb574781cfc827f2ed6dc8e050de9415d3ff1
Hamed <hamedakhlaghi@yahoo.com>
2019-10-30 22:21:09 +0330

    Tempreture high update

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,7 @@
 const request = require('request')
 
+
+
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/2ecc6e023decef5a5d3921ae6795f350/' + latitude + ',' + longitude
 
@@ -9,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.High tempreture is: ' + body.daily.data[0].temperatureHigh)
         }
     })
 }
